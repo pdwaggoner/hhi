de0c1f29474bb2d7d46234b844dc70499c62c997
Philip Waggoner <31326382+pdwaggoner@users.noreply.github.com>
2018-04-03 13:48:37 -0500

    Update hhi.R

--- hhi.R
+++ hhi.R
@@ -9,19 +9,22 @@ hhi <- function(x, s){
 }
 
 ## Then, quick plotting function with an example
-plot.hhi <- function(x,t,h){
+plot_hhi <- function(x,t,h){
+  if (!requireNamespace("ggplot2", quietly = TRUE)) {
+    stop("Package \"ggplot2\" needed for this function to work. Please install it.",
+         call. = FALSE)
+  }
 
   t <- x[ ,t]
   h <- x[ ,h]
 
-  hhi.plot <- ggplot(x, aes(t, h)) +
-    geom_point() +
-    geom_line() +
-    xlab("Time") +
-    ylab("Herfindahl-Hirschman Index") +
-    ggtitle("Herfindahl-Hirschman Index Over Time") +
-    theme_bw() +
-    theme(plot.title = element_text(hjust = 0.5))
+  hhi.plot <- ggplot2::ggplot(x, ggplot2::aes(t, h)) +
+    ggplot2::geom_point() +
+    ggplot2::geom_line() +
+    ggplot2::xlab("Time") +
+    ggplot2::ylab("Herfindahl-Hirschman Index") +
+    ggplot2::ggtitle("Herfindahl-Hirschman Index Over Time") +
+    ggplot2::theme_bw()
 
   return(hhi.plot)
 

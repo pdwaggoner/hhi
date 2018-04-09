35ba4754f9834d3deae371b4fee2079a3411a922
Philip Waggoner <31326382+pdwaggoner@users.noreply.github.com>
2018-04-09 11:22:22 -0500

    Update footwear example.R

--- footwear example.R
+++ footwear example.R
@@ -3,14 +3,13 @@ install.packages("hhi")
 library(hhi)
 
 # Next, read in Data: .txt file of US Men's Footwear Company Market Shares, 2012-2017 (Source: Euromonitor Passport)
-footwear <- read.table(".../footwear.txt") # for the .txt file, visit my GitHub and download "footwear.txt"
+footwear <- read.table(".../footwear.txt") # for the .txt file, download "footwear.txt"
 
 # Now, use the "hhi" command to calculate HHI for years 2016 and then 2017
 hhi(footwear, "ms.2016")
 hhi(footwear, "ms.2017")
 
-# Finally, to visualize hhi over time, calculate and store HHI for each year in the data set (2012-2017)
-
+# Finally, to visualize HHI over time, calculate and store HHI for each year in the data set (2012-2017)
 hhi.12 <- hhi(footwear, "ms.2012")
 hhi.13 <- hhi(footwear, "ms.2013")
 hhi.14 <- hhi(footwear, "ms.2014")
@@ -20,10 +19,8 @@ hhi.17 <- hhi(footwear, "ms.2017")
 
 # Combine and create df for plot
 hhi <- rbind(hhi.12, hhi.13, hhi.14, hhi.15, hhi.16, hhi.17)
-
 year <- c(2012, 2013, 2014, 2015, 2016, 2017)
-
 hhi.data <- data.frame(year, hhi)
 
-# Finally, generate hhi time series plot using the "plot_hhi" command
+# Finally, generate HHI time series plot using the "plot_hhi" command
 plot_hhi(hhi.data, "year", "hhi")
